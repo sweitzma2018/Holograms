096e940efc97960f515d207cae0f72206f608dc2
Sam Weitzman <sweitzma@login001.oscar.ccv.brown.edu>
2016-10-25 00:15:59 -0400

    fixed so it compiles on Linux (gcc/4.9.2 and openCV/3.0.0)

--- src/main.cpp
+++ src/main.cpp
@@ -235,7 +235,7 @@ int main(int argc, char** argv)
 #ifdef _MSC_VER
 	CreateDirectory(outFile.c_str(), NULL);
 #else
-	mkdir(outputFolder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	mkdir(settings->getOutputFolder().c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 #endif
 
 	//setup ReportWriter
